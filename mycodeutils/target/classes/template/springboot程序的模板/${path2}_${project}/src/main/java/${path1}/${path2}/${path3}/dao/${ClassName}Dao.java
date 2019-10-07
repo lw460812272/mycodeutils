917e289138ed6a4305c1c01ed9ec336a76e96935
li <wei>
2019-10-07 10:52:08 +0800

    删除文件

--- mycodeutils/target/classes/template/springboot程序的模板/${path2}_${project}/src/main/java/${path1}/${path2}/${path3}/dao/${ClassName}Dao.java
+++
@@ -1,9 +0,0 @@
-package ${pPackage}.dao;
-
-import ${pPackage}.pojo.${ClassName};
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-
-public interface ${ClassName}Dao extends JpaRepository<${ClassName},String> ,JpaSpecificationExecutor<${ClassName}> {
-}
